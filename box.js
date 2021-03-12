dc0a79eafbcf795d565320c5db1cda4fd83487a0
raashid3161 <69690167+raashid3161@users.noreply.github.com>
2021-03-11 21:26:44 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,14 @@
+class Box{
+constructor(x,y,width,height){
+var options = {isStatic:true}
+this.width = width
+this.height = height
+this.body = Matter.Bodies.rectangle(x,y,width,height, options)
+World.add(world,this.body)
+}
+
+display(){
+fill("red")
+rect(this.body.position.x,this.body.position.y, this.width, this.height)
+}
+}
